f4b1a989c13bac2497b91839c769267823cf69d6
kamildziuk <kamildziukwebdeveloper@gmail.com>
2025-02-18 18:54:20 +0100

    feature:adding file in config folder .
    creating and moving a modifiedconfiguration file (connection to MySQL database) config_reservation.php from reop hollapolla_restaurant_reservation_system to merge them into one project

---
+++ src/config/config_reservation.php
@@ -0,0 +1,29 @@
+<?php
+
+try{
+$dbhost = '';
+$dbname='';
+$dbuser = '';
+$dbpass = '';
+$pdo = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
+}
+catch(PDOException $e)
+{
+echo "ERRPR:" . $e -> getMessage() . "<br>";
+};
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
