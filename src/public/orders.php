7f00ba0177d7034f2dafcd91233066783b16eae1
kamildziuk <kamildziukwebdeveloper@gmail.com>
2025-02-18 17:28:43 +0100

    feature: transferring an public files from the ordering system
    moving modified displaying_orders.php and orders.php files to public folder of hollapolla_restaurant_ordering_system repository to merge them into one project

---
+++ src/public/orders.php
@@ -0,0 +1,346 @@
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>HollaPolla - Orders</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="../styles/orders.css">
+
+  <link rel="icon" type="image/png" href="assets/images/browserpictogram.png">
+  <link rel="apple-touch-icon" href="assets/images/icon-96x96.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="assets/images/icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="167x167" href="assets/images/icon-167x167.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="assets/images/icon-180x180.png">
+  <link rel="manifest" href="manifest.json">
+  
+  
+
+
+</head>
+
+<body>
+  <!-- gtranslate start -->
+  <div class="gtranslate_wrapper"></div>
+<script>window.gtranslateSettings = {"default_language":"en","native_language_names":true,"detect_browser_language":true,"languages":["en","pl","nl","fr"],"wrapper_selector":".gtranslate_wrapper","switcher_horizontal_position":"right","float_switcher_open_direction":"right"}</script>
+<script src="https://cdn.gtranslate.net/widgets/latest/float.js" defer></script>
+
+  <!-- gtranslate start  end-->
+
+<div class="nav_headar">
+  <div class="container">
+    <nav class="navbar">
+      <a href="https://hollapolla.nl/" class="nav-logo"><img    class="logoHP"  src="assets/images/logoHP.webp">  </a>
+      <ul class="nav-menu">
+
+      <li class="nav-item">
+              <a href="https://hollapolla.nl/" class="nav-link">Menu</a>     
+       </li>
+
+<li class="nav-item">
+  <a href="https://reservations.hollapolla.nl/" class="nav-link" id="openReservation"   style="color:#be8040"    >Reservations</a>        
+   </li>
+  <li class="nav-item">
+    <a href="https://orders.hollapolla.nl/" class="nav-link" id="openReservations"   style="color:#f4d135"    >Orders online</a>        
+  </li>
+
+<li class="nav-item">
+  <a style="color:#cdcdcd"   onclick="window.location.href='tel:+31 6 58723606'">+31 6 58723606</a>    
+</li>
+
+
+
+    <li class="nav-item">
+  <a href="https://www.facebook.com/profile.php?id=61555700734528" class="fa fa-facebook"></a>
+  <a href="https://www.instagram.com/hollapolla66/?utm_source=qr&igsh=MXFzdG11cWE0OXhvNg%3D%3D" class="fa fa-instagram"></a>
+  </li>
+
+
+        </ul>
+      <div class="hamburger">
+        <span class="bar"></span>
+        <span class="bar"></span>
+        <span class="bar"></span>
+      </div>
+    </nav>
+  </div>
+</div>
+
+
+
+<div class="orderSummaryBackground">
+  
+<div class="orderSummary">
+<div class="codeNumber"> </div>
+
+<div class="textStatus">
+   </div>
+
+
+
+</div>
+  
+</div>
+
+<div id="ordersForm">
+  <div class="information"></div><br> 
+  <input type="text" id="first_name" placeholder=" Last name" >
+  <input type="email" id="email" name="email" placeholder="Email"> 
+  <input type="number" id="phone" name="phone" placeholder=" Phone" >
+  <textarea    rows="2" cols="10"  id="addres" name="addres" placeholder=" Addres"  ></textarea>
+
+  <div id="contenerOrderInfo">
+  
+    <div class="boxOrderInfo">
+      
+    <div class="numbersOrderLocation">Numbers order</div>
+    <div class="quantityLocation">Quantity</div>
+    
+  </div>
+  </div>
+
+
+  <div class="information display" style="color: #ff0000; font-size: 12px;">To send an order, click on the name of the dish on the menu or enter the dish number in the message.</div>
+
+
+<textarea    rows="4" cols="10"    id="message" name="message" placeholder=" Message"></textarea>
+
+  <input type="submit" id="submit" value="submit">
+
+  <div class="information" style="color: #be8040;">Accept the rules to send</div> 
+  <label class="containerCheck">I consent to the processing by HollaPolla of my personal data contained in this contact form for the purpose and to the extent necessary to process the application.
+    <input type="checkbox"  name="checkbox" >
+    <span class="checkmark"></span>
+  </label>
+  <label class="containerCheck">I consent to the processing by HollaPolla of my personal data contained in this contact form for the purpose of sending me commercial offers for the company's own products electronically.
+    <input type="checkbox" name="checkbox" >
+    <span class="checkmark"></span>
+  </label>
+  <label class="containerCheck"> I consent to the processing by HollaPolla of my personal data contained in this contact form for the purpose of telephone contact by the company's representatives in matters related to the commercial offer for its own products.
+    <input type="checkbox" name="checkbox" >
+    <span class="checkmark"></span>
+  </label>
+
+  </div> 
+
+</div>
+</div>
+<div id="contenerImage">
+  <div class="information">When ordering through our website you will receive a <b   style="color: #be8040;">10%</b> discount. For orders over<b   style="color:#f4d135"> €50 free delivery</b>, under €4.<br> Payment can be made upon receipt of the order.  Payment by <b   style="color: #be8040;">card </b>or <b   style="color: #009e20;">cash</b>.<br><br>Click on the name of a <b  style="color:#f4d135">dish </b>in the menu to add it to your order.</div> <br>
+  <svg id="imageOrdersSvg0" version="1.0" xmlns="http://www.w3.org/2000/svg"
+  width="1587.000000pt" height="2245.000000pt" viewBox="0 0 1587.000000 2245.000000"
+  preserveAspectRatio="xMidYMid meet">
+ 
+ <g transform="translate(0.000000,2245.000000) scale(0.100000,-0.100000)"
+ fill="#000000" stroke="none">
+ <path  class="position" d="M2604 16887 c-2 -7 -3 -118 -2 -247 l3 -235 5367 -3 5367 -2 11 20
+ c7 14 10 95 8 241 -2 182 -6 221 -18 229 -9 6 -2019 10 -5373 10 -4323 0
+ -5359 -2 -5363 -13z"/>
+ <path class="position" d="M3476 13324 c-14 -13 -16 -48 -16 -229 0 -181 2 -216 16 -229 14 -15
+ 431 -16 4435 -16 3028 0 4426 3 4440 10 18 10 19 23 19 230 0 185 -2 221 -16
+ 234 -14 15 -431 16 -4439 16 -4008 0 -4425 -1 -4439 -16z"/>
+ <path class="position" d="M6446 9144 c-14 -13 -16 -48 -16 -229 0 -181 2 -216 16 -229 14 -14
+ 160 -16 1457 -16 889 0 1448 4 1457 10 12 7 16 43 21 172 6 188 -1 285 -22
+ 299 -8 5 -605 9 -1456 9 -1297 0 -1443 -2 -1457 -16z"/>
+ <path class="position" d="M4416 7454 c-14 -13 -16 -48 -16 -229 0 -181 2 -216 16 -229 14 -15
+ 355 -16 3599 -16 3244 0 3585 1 3599 16 14 13 16 48 16 229 0 181 -2 216 -16
+ 229 -14 15 -355 16 -3599 16 -3244 0 -3585 -1 -3599 -16z"/>
+ <path class="position" d="M5396 5794 c-14 -13 -16 -48 -16 -229 0 -181 2 -216 16 -229 14 -14
+ 257 -16 2529 -16 2272 0 2515 2 2529 16 24 23 24 435 0 458 -14 14 -257 16
+ -2529 16 -2272 0 -2515 -2 -2529 -16z"/>
+ <path class="position" d="M2174 4126 c-18 -13 -19 -28 -19 -231 0 -203 1 -217 19 -231 17 -12
+ 821 -14 5787 -14 5230 0 5769 1 5783 16 14 13 16 48 16 229 0 181 -2 216 -16
+ 229 -14 15 -553 16 -5783 16 -4964 0 -5770 -2 -5787 -14z"/>
+ </g>
+ </svg>
+ 
+ <svg version="1.0" id="imageOrdersSvg1" xmlns="http://www.w3.org/2000/svg"
+  width="1587.000000pt" height="2245.000000pt" viewBox="0 0 1587.000000 2245.000000"
+  preserveAspectRatio="xMidYMid meet">
+ 
+ <g transform="translate(0.000000,2245.000000) scale(0.100000,-0.100000)"
+ fill="#000000" stroke="none">
+ <path  class="position" d="M2846 18104 c-24 -23 -24 -465 0 -488 14 -15 490 -16 5084 -16 4594
+ 0 5070 1 5084 16 24 23 24 465 0 488 -14 15 -490 16 -5084 16 -4594 0 -5070
+ -1 -5084 -16z"/>
+ <path  class="position" d="M2376 16204 c-24 -23 -24 -455 0 -478 14 -15 418 -16 4297 -16 2355
+ -1 4880 -2 5610 -3 1171 -2 1329 0 1337 13 5 8 10 116 10 242 0 193 -2 229
+ -16 242 -14 15 -538 16 -5619 16 -5081 0 -5605 -1 -5619 -16z"/>
+ <path  class="position" d="M2626 14464 c-14 -13 -16 -48 -16 -229 0 -181 2 -216 16 -229 14 -15
+ 505 -16 5259 -16 4754 0 5245 1 5259 16 24 23 24 435 0 458 -14 15 -505 16
+ -5259 16 -4754 0 -5245 -1 -5259 -16z"/>
+ <path  class="position" d="M6006 12594 c-13 -13 -16 -46 -16 -209 0 -163 3 -196 16 -209 14 -14
+ 185 -16 1717 -17 2030 -2 1978 -3 1995 19 9 11 12 70 12 208 0 162 -3 195 -16
+ 208 -14 14 -196 16 -1854 16 -1658 0 -1840 -2 -1854 -16z"/>
+ <path  class="position" d="M2806 10824 c-14 -13 -16 -52 -16 -274 0 -222 2 -261 16 -274 14 -15
+ 499 -16 5184 -16 4685 0 5170 1 5184 16 14 13 16 52 16 274 0 222 -2 261 -16
+ 274 -14 15 -499 16 -5184 16 -4685 0 -5170 -1 -5184 -16z"/>
+ <path  class="position" d="M5466 9994 c-14 -13 -16 -52 -16 -274 0 -222 2 -261 16 -274 14 -14
+ 245 -16 2389 -16 2144 0 2375 2 2389 16 24 23 24 525 0 548 -14 14 -245 16
+ -2389 16 -2144 0 -2375 -2 -2389 -16z"/>
+ <path  class="position" d="M4844 8103 c-16 -16 -16 -38 -10 -248 5 -199 8 -232 23 -242 13 -10
+ 636 -13 3034 -13 2730 0 3019 1 3033 16 14 13 16 50 16 244 0 194 -2 231 -16
+ 244 -14 15 -304 16 -3039 16 -2849 0 -3025 -1 -3041 -17z"/>
+ <path  class="position" d="M3076 6294 c-14 -13 -16 -51 -16 -259 0 -208 2 -246 16 -259 14 -15
+ 457 -16 4728 -16 3754 0 4715 3 4728 13 30 22 38 89 38 299 0 174 -2 209 -16
+ 222 -14 15 -458 16 -4739 16 -4281 0 -4725 -1 -4739 -16z"/>
+ <path   class="position" d="M5406 4574 c-14 -13 -16 -55 -16 -299 0 -244 2 -286 16 -299 14 -14
+ 260 -16 2563 -16 2046 0 2551 3 2565 13 15 11 17 40 20 301 2 238 0 290 -11
+ 302 -12 12 -376 14 -2568 14 -2308 0 -2555 -2 -2569 -16z"/>
+ </g>
+ </svg>
+ <svg  id="imageOrdersSvg2" version="1.0" xmlns="http://www.w3.org/2000/svg"
+  width="1587.000000pt" height="2245.000000pt" viewBox="0 0 1587.000000 2245.000000"
+  preserveAspectRatio="xMidYMid meet">
+ 
+ <g transform="translate(0.000000,2245.000000) scale(0.100000,-0.100000)"
+ fill="#000000" stroke="none">
+ <path class="position" d="M6296 19104 c-24 -23 -24 -435 0 -458 14 -15 176 -16 1677 -14 l1662
+ 3 3 223 c1 142 -1 230 -8 242 -11 20 -29 20 -1665 20 -1490 0 -1655 -2 -1669
+ -16z"/>
+ <path class="position" d="M4646 16504 c-14 -13 -16 -48 -16 -224 0 -176 2 -211 16 -224 14 -15
+ 326 -16 3284 -16 2958 0 3270 1 3284 16 14 13 16 48 16 224 0 176 -2 211 -16
+ 224 -14 15 -326 16 -3284 16 -2958 0 -3270 -1 -3284 -16z"/>
+ <path class="position" d="M3176 13044 c-14 -13 -16 -56 -16 -319 0 -263 2 -306 16 -319 14 -15
+ 460 -16 4754 -16 4294 0 4740 1 4754 16 14 13 16 56 16 319 0 263 -2 306 -16
+ 319 -14 15 -460 16 -4754 16 -4294 0 -4740 -1 -4754 -16z"/>
+ <path class="position" d="M2406 9584 c-14 -13 -16 -56 -16 -319 0 -263 2 -306 16 -319 14 -15
+ 528 -16 5504 -16 4976 0 5490 1 5504 16 14 13 16 56 16 312 0 163 -4 303 -9
+ 310 -4 7 -26 18 -47 22 -23 6 -2281 10 -5496 10 -4947 0 -5458 -1 -5472 -16z"/>
+ <path class="position"  d="M4996 3784 c-14 -13 -16 -47 -16 -219 0 -189 1 -205 18 -214 12 -6
+ 348 -11 912 -13 l893 -3 -7 119 c-3 66 -6 163 -6 217 0 75 -4 101 -16 113 -14
+ 14 -108 16 -889 16 -781 0 -875 -2 -889 -16z"/>
+ <path class="position" d="M8925 3770 c-891 -3 -1626 -9 -1632 -13 -10 -6 -13 -60 -13 -212 0
+ -172 2 -206 16 -219 14 -14 175 -16 1623 -16 884 0 1642 -3 1684 -6 l77 -7 0
+ 242 0 241 -67 -2 c-38 -2 -797 -5 -1688 -8z"/>
+ <path class="position" d="M6496 2924 c-23 -22 -24 -405 -1 -427 12 -13 204 -16 1457 -19 794
+ -2 1444 -3 1445 -3 2 1 0 99 -4 219 -5 167 -10 221 -20 232 -12 12 -227 14
+ -1438 14 -1281 0 -1425 -2 -1439 -16z"/>
+ </g>
+ </svg>
+ 
+ 
+ <svg id="imageOrdersSvg3" version="1.0" xmlns="http://www.w3.org/2000/svg"
+  width="1587.000000pt" height="2245.000000pt" viewBox="0 0 1587.000000 2245.000000"
+  preserveAspectRatio="xMidYMid meet">
+ 
+ <g transform="translate(0.000000,2245.000000) scale(0.100000,-0.100000)"
+ fill="#000000" stroke="none">
+ <path class="position" d="M3756 16894 c-14 -13 -16 -48 -16 -229 0 -181 2 -216 16 -229 16 -16
+ 843 -18 6309 -16 413 0 1003 0 1312 0 497 0 563 2 577 16 14 13 16 48 16 229
+ 0 181 -2 216 -16 229 -14 15 -400 16 -4099 16 -3699 0 -4085 -1 -4099 -16z"/>
+ <path  class="position" d="M4936 14914 c-14 -13 -16 -48 -16 -224 0 -176 2 -211 16 -224 14 -15
+ 300 -16 3004 -16 2704 0 2990 1 3004 16 24 23 24 425 0 448 -14 15 -300 16
+ -3004 16 -2704 0 -2990 -1 -3004 -16z"/>
+ <path  class="position" d="M4826 13184 c-14 -13 -16 -47 -16 -214 0 -167 2 -201 16 -214 14 -15
+ 307 -16 3079 -16 2772 0 3065 1 3079 16 24 23 24 405 0 428 -14 15 -307 16
+ -3079 16 -2772 0 -3065 -1 -3079 -16z"/>
+ <path class="position" d="M4876 11414 c-13 -13 -16 -46 -16 -204 0 -158 3 -191 16 -204 14 -15
+ 304 -16 3044 -16 2740 0 3030 1 3044 16 13 13 16 46 16 204 0 158 -3 191 -16
+ 204 -14 15 -304 16 -3044 16 -2740 0 -3030 -1 -3044 -16z"/>
+ <path class="position" d="M5506 6474 c-13 -13 -16 -44 -16 -179 0 -135 3 -166 16 -179 14 -14
+ 243 -16 2369 -16 2126 0 2355 2 2369 16 13 13 16 44 16 179 0 135 -3 166 -16
+ 179 -14 14 -243 16 -2369 16 -2126 0 -2355 -2 -2369 -16z"/>
+ <path class="position" d="M5026 4974 c-13 -13 -16 -44 -16 -184 0 -140 3 -171 16 -184 14 -14
+ 290 -16 2894 -16 2604 0 2880 2 2894 16 13 13 16 45 16 189 0 162 -1 175 -19
+ 185 -14 7 -932 10 -2895 10 -2600 0 -2876 -2 -2890 -16z"/>
+ <path class="position" d="M5876 3404 c-23 -23 -23 -315 0 -338 14 -14 136 -16 1200 -16 1171 0
+ 1184 0 1204 20 18 18 20 33 20 175 0 142 -1 155 -19 165 -13 7 -407 10 -1205
+ 10 -1064 0 -1186 -2 -1200 -16z"/>
+ <path  class="position" d="M8635 3411 c-19 -8 -20 -15 -15 -175 4 -133 8 -169 20 -176 22 -14
+ 1838 -13 1879 1 17 6 36 19 41 29 6 10 10 82 10 159 0 114 -3 142 -16 155 -14
+ 14 -114 16 -957 15 -518 0 -951 -4 -962 -8z"/>
+ <path class="position"  d="M10746 3404 c-13 -13 -16 -43 -16 -169 0 -126 3 -156 16 -169 14 -14
+ 84 -16 623 -16 475 0 611 3 625 13 15 11 17 33 18 175 2 133 -1 165 -13 173
+ -8 5 -274 9 -626 9 -543 0 -613 -2 -627 -16z"/>
+ <path class="position"  d="M6500 1880 c-31 -20 -31 -330 0 -350 9 -6 622 -10 1592 -10 1420 0
+ 1578 2 1592 16 23 23 23 315 0 338 -14 14 -172 16 -1592 16 -962 0 -1583 -4
+ -1592 -10z"/>
+ <path class="position"  d="M10016 1874 c-13 -13 -16 -43 -16 -174 0 -147 1 -160 19 -170 31 -16
+ 1382 -14 1403 3 14 10 17 40 20 185 l3 172 -707 0 c-629 0 -708 -2 -722 -16z"/>
+ </g>
+ </svg>
+ 
+ <svg id="imageOrdersSvg4" version="1.0" xmlns="http://www.w3.org/2000/svg"
+  width="1587.000000pt" height="2245.000000pt" viewBox="0 0 1587.000000 2245.000000"
+  preserveAspectRatio="xMidYMid meet">
+ 
+ <g transform="translate(0.000000,2245.000000) scale(0.100000,-0.100000)"
+ fill="#000000" stroke="none">
+ <path class="position" d="M6388 17189 c-16 -9 -18 -29 -18 -189 0 -165 1 -180 19 -189 12 -7
+ 397 -11 1112 -11 1713 -1 1813 0 1829 10 11 7 17 40 22 137 11 176 11 177 -6
+ 217 l-15 36 -1463 0 c-975 -1 -1469 -4 -1480 -11z"/>
+ <path class="position" d="M5986 15404 c-13 -13 -16 -45 -16 -194 0 -149 3 -181 16 -194 13 -14
+ 50 -16 247 -16 1101 0 2932 0 3229 0 320 0 369 2 382 16 24 23 24 365 0 388
+ -14 14 -203 16 -1929 16 -1726 0 -1915 -2 -1929 -16z"/>
+ <path class="position" d="M5026 13584 c-14 -13 -16 -47 -16 -219 0 -172 2 -206 16 -219 14 -14
+ 273 -16 2704 -16 2431 0 2690 2 2704 16 24 23 24 415 0 438 -14 14 -273 16
+ -2704 16 -2431 0 -2690 -2 -2704 -16z"/>
+ <path class="position" d="M6176 11784 c-13 -13 -16 -45 -16 -194 0 -149 3 -181 16 -194 14 -14
+ 170 -16 1574 -16 1404 0 1560 2 1574 16 24 23 24 365 0 388 -14 14 -170 16
+ -1574 16 -1404 0 -1560 -2 -1574 -16z"/>
+ <path class="position" d="M6036 9974 c-13 -13 -16 -45 -16 -194 0 -149 3 -181 16 -194 14 -14
+ 183 -16 1709 -16 1526 0 1695 2 1709 16 24 23 24 365 0 388 -14 14 -183 16
+ -1709 16 -1526 0 -1695 -2 -1709 -16z"/>
+ <path class="position" d="M2436 8204 c-14 -13 -16 -48 -16 -224 0 -176 2 -211 16 -224 14 -15
+ 533 -16 5562 -16 3470 0 5553 4 5562 10 12 7 16 48 21 222 6 203 5 214 -14
+ 231 -18 16 -298 17 -5567 17 -5031 0 -5550 -1 -5564 -16z"/>
+ </g>
+ </svg>
+ 
+ <svg id="imageOrdersSvg5" version="1.0" xmlns="http://www.w3.org/2000/svg"
+  width="1587.000000pt" height="2245.000000pt" viewBox="0 0 1587.000000 2245.000000"
+  preserveAspectRatio="xMidYMid meet">
+ 
+ <g transform="translate(0.000000,2245.000000) scale(0.100000,-0.100000)"
+ fill="#000000" stroke="none">
+ <path class="position" d="M5800 17039 l0 -282 53 7 c28 3 892 6 1919 6 1683 0 1868 2 1882 16
+ 24 23 24 495 0 518 -14 14 -203 16 -1935 16 l-1919 0 0 -281z"/>
+ <path class="position" d="M3126 15334 c-14 -13 -16 -50 -16 -252 0 -132 5 -244 10 -252 6 -10
+ 33 -17 85 -20 41 -3 2139 -3 4661 0 4061 4 4586 7 4594 20 5 8 10 120 10 252
+ 0 202 -2 239 -16 252 -14 15 -451 16 -4664 16 -4213 0 -4650 -1 -4664 -16z"/>
+ <path class="position" d="M3270 13210 c-12 -8 -16 -52 -20 -262 -3 -139 -4 -256 -2 -260 2 -5
+ 2102 -8 4667 -8 4226 0 4665 1 4679 16 24 23 24 485 0 508 -14 15 -451 16
+ -4662 16 -2905 0 -4653 -4 -4662 -10z"/>
+ <path class="position" d="M4817 11221 c-2143 -5 -2429 -8 -2437 -21 -9 -14 -17 -235 -12 -325
+ 6 -116 6 -162 -1 -181 l-7 -21 282 4 c156 2 320 8 364 15 48 6 93 7 110 2 16
+ -4 2245 -11 4954 -14 2709 -4 4930 -7 4935 -7 6 -1 88 2 183 5 138 6 175 10
+ 182 22 5 8 10 121 10 252 0 202 -2 239 -16 252 -14 14 -252 16 -2468 16 -1349
+ 0 -2722 2 -3052 3 -329 2 -1692 1 -3027 -2z"/>
+ <path class="position" d="M2246 9104 c-9 -8 -16 -26 -16 -39 0 -13 -8 -28 -17 -34 -17 -9 -17
+ -13 0 -74 12 -47 17 -104 17 -205 0 -116 3 -143 16 -157 15 -15 271 -16 3123
+ -15 1708 1 4200 1 5537 0 1338 0 2439 2 2447 6 9 3 18 13 21 22 7 25 10 137 8
+ 319 -2 132 -5 164 -18 177 -14 15 -533 16 -5559 16 -5026 0 -5545 -1 -5559
+ -16z"/>
+ <path  class="position" d="M3736 7005 c-23 -23 -28 -82 -24 -298 3 -183 5 -200 22 -213 17 -12
+ 595 -14 4107 -14 3703 0 4089 1 4103 16 24 23 24 485 0 508 -14 15 -400 16
+ -4104 16 -3635 0 -4090 -2 -4104 -15z"/>
+ </g>
+ </svg>
+ 
+<img id="imageOrders0"  src="assets/images/menu_image_ordering_nl_0.webp">
+<img id="imageOrders1"  src="assets/images/menu_image_ordering_nl_1.webp">
+<img id="imageOrders2"  src="assets/images/menu_image_ordering_nl_2.webp">
+<img id="imageOrders3"  src="assets/images/menu_image_ordering_nl_3.webp">
+<img id="imageOrders4"  src="assets/images/menu_image_ordering_nl_4.webp">
+<img id="imageOrders5"  src="assets/images/menu_image_ordering_nl_5.webp">
+
+
+</div>
+
+
+
+<!-- email  connect start -->
+<script src="https://smtpjs.com/v3/smtp.js"></script>
+<!-- email  connect end -->
+<script src="../scripts/orders.js" defer></script>
+<script src="../scripts/script.js" defer></script>
+
+
+</body>
+</html>
