23c2afde15960041c64e844638691d0ba899ee00
kamildziuk <kamildziukwebdeveloper@gmail.com>
2025-02-19 13:55:27 +0100

    feature: swap menu images and text
    replacing the images of the menu with text with the name of the dish, information about it and the price. Inside the div with the id containerImage a classy position was created with values ​​corresponding to the price of the dishes, inside the classy position there is a class pricStyle which styles the price in the menu

--- src/public/orders.php
+++ src/public/orders.php
@@ -131,11 +131,80 @@
 </div>
 </div>
 <div id="contenerImage">
-  <div class="information">When ordering through our website you will receive a <b   style="color: #be8040;">10%</b> discount. For orders over<b   style="color:#f4d135"> €50 free delivery</b>, under €4.<br> Payment can be made upon receipt of the order.  Payment by <b   style="color: #be8040;">card </b>or <b   style="color: #009e20;">cash</b>.<br><br>Click on the name of a <b  style="color:#f4d135">dish </b>in the menu to add it to your order.</div> <br>
-
-
-
+<div class="information">When ordering through our website you will receive a <b   style="color: #be8040;">10%</b> discount. For orders over<b   style="color:#f4d135"> €50 free delivery</b>, under €4.<br> Payment can be made upon receipt of the order.  Payment by <b   style="color: #be8040;">card </b>or <b   style="color: #009e20;">cash</b>.<br><br>Click on the name of a <b  style="color:#f4d135">dish </b>in the menu to add it to your order.
 </div>
+<h3>Voorgerechten</h3>
+<div class="position"  value="12">Rundertartaar met boerenbrood <div class="pricStyle">€ 12 </div></div>
+<div class="info_about_dish">Beschikbaar vrijdag,zaterdag.Zondag / komkommer, witte ui, hampignons, eigeel, boter/</div>
+
+<div class="position" value="6">6 toast met knoflookboter<div class="pricStyle">€ 6</div></div>
+
+
+<h3>Soepen</h3>
+<div class="position" value="6">Kippensoep<div class="pricStyle">€ 6</div></div>
+<div class="position" value="8">Zure soep met worst en ei<div class="pricStyle">€ 8</div></div>
+<div class="position" value="7">Uiensoep met kaas<div class="pricStyle">€ 7</div></div>
+<div class="position" value="9">Pittige soep met getrokken varkensvlees<div class="pricStyle">€ 9</div></div>
+
+<h3>Dinersets</h3>
+<div class="position" value="28">Varkensvlees met paddestoelsaus*<div class="pricStyle">€ 28</div></div>
+<div class="position" value="29">Rundvleesrol en silesische dumplings<div class="pricStyle">€ 29</div></div>
+<div class="position" value="24">Varkensschnitzel op een half bord*<div class="pricStyle">€ 24</div></div>
+<div class="position" value="22">Kipschnitzel<div class="pricStyle">€ 22</div></div>
+<div class="position" value="22">Witte kool gevuld met gehaktrijst in tomatensaus*<div class="pricStyle">€ 22</div></div>
+<div class="position" value="24">Zalm met dille saus*<div class="pricStyle">€ 24</div></div>
+<div class="position" value="22">Kabeljauwfilet in bier gepaneerd *<div class="pricStyle">€ 22</div></div>
+<div class="position" value="22">Hongaarse taart<div class="pricStyle">€ 22</div></div>
+
+<h3>Salades</h3>
+<div class="position" value="7">Huis salade<div class="pricStyle">€ 7</div></div>
+<div class="info_about_dish">/paprika, tomaat, rode ui, groene komkommer, vinaigrette/</div>
+
+<div class="position" value="18">Ceasar salade met kip<div class="pricStyle">€ 18</div></div>
+<div class="info_about_dish">/ijsbergsla, komkommer, paprika, rode ui, gedroogde tomaat, parmezaan, knoflookcroutons, vinaigrettesaus/</div>
+
+
+<div class="position" value="18">Salade met varkenshaas puntjes<div class="pricStyle">€ 18</div></div>
+<div class="info_about_dish">/ijsbergsla, komkommer, paprika, rode ui, kappertjes, knoflookcroutons, azuurblauwe saus, vinaigrettesaus/</div>
+
+<div class="position" value="18">Salade met gerookte zalm/feta/tofu<div class="pricStyle">€ 18</div></div>
+<div class="info_about_dish">/ijsbergsla, komkommer, paprika, rode ui, knoflookcroutons, vinaigrettesaus/</div>
+
+
+
+<h3>Kleine salades</h3>
+<div class="position" value="4">Bieten<div class="pricStyle">€ 4</div></div>
+<div class="position" value="4">Huis salade<div class="pricStyle">€ 4</div></div>
+<div class="position" value="4">Coleslaw<div class="pricStyle">€ 4</div></div>
+<h3>Pasta / dumplings</h3>
+<div class="position" value="15">Spinazie en kaas tagliatelle <div class="pricStyle">€ 15</div></div>
+<div class="position" value="17">Russische dumplings<div class="pricStyle">€ 17</div></div>
+<div class="position" value="17">Dumplings met vlees<div class="pricStyle">€ 17</div></div>
+<div class="position" value="6">Silesische dumplings<div class="pricStyle">€ 6</div></div>
+
+<h3>Extra's</h3>
+<div class="position" value="4">Aardappelen puree<div class="pricStyle">€ 4</div></div>
+<div class="position" value="4">Gebakken aardappelen<div class="pricStyle">€ 4</div></div>
+<div class="position" value="4">Sauzen <div class="pricStyle">€ 4</div></div>
+<div class="info_about_dish">Gebraden, tomaat, dille</div>
+
+<h3>Kindermenu</h3>
+<div class="position" value="6">Kippensoep<div class="pricStyle">€ 6</div></div>
+<div class="position" value="6">Tomatensoep<div class="pricStyle">€ 6</div></div>
+<div class="position" value="8">Gebakken vissticks*<div class="pricStyle">€ 8</div></div>
+<div class="position" value="8">Kipnuggets<div class="pricStyle">€ 8</div></div>
+<div class="position" value="22">Kipschnitzel<div class="pricStyle">€ 22</div></div>
+<div class="position" value="9">Pannenkoeken met roomkaas en fruit<div class="pricStyle">€ 9</div></div>
+
+<h3>Desserts</h3>
+<div class="position" value="4">1 bolletje ijs<div class="pricStyle">€ 4</div></div>
+<div class="position" value="8">Ijsdessert met karamel en noten<div class="pricStyle">€ 8</div></div>
+<div class="position" value="8">Chocoladetaart met slagroom en ijs<div class="pricStyle">€ 8</div></div>
+<div class="position" value="8">Warme appeltaart met slagroom en ijs<div class="pricStyle">€ 8</div></div>
+<div class="position" value="8">Kwarktaart met chocolade<div class="pricStyle">€ 8</div></div>
+</div>
+</div>
+
 
 
 
